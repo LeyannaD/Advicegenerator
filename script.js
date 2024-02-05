5e2c3ab34b0499dbdc4e86c41c696d17f9df90d0
LeyannaD <96096629+LeyannaD@users.noreply.github.com>
2024-02-05 01:34:23 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,27 @@
+//create XHR request
+let request = new XMLHttpRequest();
+//store api in url in a variable 
+let url = "https://api.adviceslip.com/advice";
+//perpare the request 
+request.open("GET", url, true);
+//create onLoad event handler
+request.onload = function() {
+  
+  let data = JSON.parse(this.response);
+  
+};
+request.onload = function() {
+
+//parse JSON data and make it readable 
+  let data = JSON.parse(this.response);
+  if(request.status >= 200 && request.status < 400) {
+    $("#advice").text(data["slip"].advice);
+  }
+
+  
+};
+  
+
+
+//send request
+request.send();
